16717dc1a37289eccbeeb49e2f15e009e4f6bede
Sebastian <sebbe@kvarnaberget.se>
2013-03-16 12:26:34 +0000

    Added way to get the raw path

--- model/Path.php
+++ model/Path.php
@@ -7,6 +7,7 @@ class Path {
 	private $filename = false;
 	private $controller = false;
 	private $args = NULL;
+	private $raw_path;
 
 	static public function from_path_info() {
 		$parts = isset($_SERVER['PATH_INFO']) ? explode('/', rtrim($_SERVER['PATH_INFO'], '/')) : array('', 'main');
@@ -20,10 +21,12 @@ class Path {
 		$path->controller = basename(array_shift($parts));
 		$path->args = $parts;
 		$path->filename = "controllers/{$path->controller}.php";
+		$path->raw_path = isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : "main";
 		return $path;
 	}
 
 	public function filename(){ return $this->filename; }
 	public function controller(){ return $this->controller; }
 	public function args(){ return $this->args; }
+	public function raw_path(){ return $this->raw_path; }
 }
