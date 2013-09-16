d4a0221576a32aca1e82d24162e3f98f6bc7369a
cpluss <sebastian@cpluss.se>
2013-09-16 20:13:57 +0200

    Update guidelines for creating new databasesites

--- view/admin/add.php
+++ view/admin/add.php
@@ -1,6 +1,31 @@
 <h2> Lägg till sida </h2>
 <p> Sidorna använder <a href="http://daringfireball.net/projects/markdown/"> Markdown</a>, guide till syntax är <a href="http://daringfireball.net/projects/markdown/syntax"> här</a>. </p>
-<p class="info"> Notera: om du lämnar Positionen tom läggs den automatiskt längst bak. </p>
+<p> För att bygga submenyer (dvs. andra gradens menyer eller så kallade dropdowns, en meny i en meny) manipulerar du "Länken" på ett sådant vis. Till exempel, för att bygga E-Sports <i>länknät</i> ser länkarna ut såhär:<p>
+<p>
+<table class="table">
+    <tr>
+        <th> Länk </th>
+        <th> Beskrivning </th>
+    </tr>
+    <tr>
+        <td> /esport </td>
+        <td> Basen, det som kommer synas i menyn ovan. </td>
+    </tr>
+    <tr>
+        <td> /esport/LoL </td>
+        <td> En undersida till E-Sport, dvs. League of Legends, som automatiskt kommer hamna under '/esport' i menyn. </td>
+    </tr>
+    <tr>
+        <td> /esport/sc2 </td>
+        <td> En undersida till E-Sport, dvs. Starcraft 2, som automatiskt kommer hamna under '/esport' i menyn. </td>
+    </tr>
+    <tr> 
+        <td> osv </td>
+        <td> ... </td>
+    </tr>
+</table>
+</p>
+<p class="alert alert-info well-sm"> <b>Notera:</b> om du lämnar positionsfältet tomt läggs det automatiskt längst bak. </p>
 
 <form id="form" method="post" action="/admin/add">
     <div style="padding-top: 5px; padding-bottom: 5px;" class="input-group">
@@ -24,6 +49,9 @@
     <a style="margin-top: 15px;" class="btn btn-warning" href="#" onclick="preview()">Förhandsgranska</a>
     <input style="margin-top: 15px;" class="btn btn-info" type="submit" value="Spara"/>
 
+    <br>
+    <a style="margin-top: 15px;" class="btn btn-default" href="/admin/edit">Tillbaka</a>
+
 </form>
 
 <script>
@@ -32,7 +60,6 @@
 			alert("Du måste ange ett namn på den nya sidan.");
 			return false;
 		}
-		//TODO: check for digits in #order
 		if($("#href").val() == "") {
 			alert("Du måste ange en länk på den nya sidan.");
 			return false;
