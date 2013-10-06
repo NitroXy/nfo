75349ce17b1c69f96ea88b72a5d66e62a8ff51a8
cpluss <sebastian@cpluss.se>
2013-10-06 21:24:21 +0200

    You are now able to delete timetable entries

---
+++ view/admin/timetable_del.php
@@ -0,0 +1,8 @@
+<h2> Ta bort nyhetselement </h2>
+<p> Vill du verkligen ta bort nyhetselementet? </p>
+<form method="post" action="/admin/timetable_del/<?=$id?>">
+	<input class="btn btn-success" type="submit" value="Ja"/>
+</form>
+<form method="post" action="/admin/timetable">
+	<input class="btn btn-danger" type="submit" value="Nej"/>
+</form>
