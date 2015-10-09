3c5cd4eafccdcee2a55ea392b2f4cf614e660921
David Sveningsson <ext@sidvind.com>
2015-10-10 01:54:15 +0200

    path: migrate changes from nitroxy-web
    makes path format aware

--- model/Path.php
+++ model/Path.php
@@ -4,26 +4,36 @@ class Path {
 	private $filename = false;
 	private $controller = false;
 	private $args = NULL;
+	private $format = NULL;
 
-	static public function from_path_info() {
+	/**
+	 * Use `from_path_info`.
+	 */
+	private function __construct(){
+
+	}
+
+	static public function from_path_info(){
 		global $repo_root;
 
 		$parts = isset($_SERVER['PATH_INFO']) ? explode('/', rtrim($_SERVER['PATH_INFO'], '/')) : array('', 'timetable');
 		array_shift($parts);
 		//array_shift($parts);
+
 		$filename = array_pop($parts);
 		$pathinfo = pathinfo($filename);
 		array_push($parts, $pathinfo['filename']);
-
 		$path = new Path;
 		$path->controller = basename(array_shift($parts));
 		$path->args = $parts;
 		$path->filename = "$repo_root/controllers/{$path->controller}.php";
-		$path->raw_path = isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : "timetable";
+		$path->format = isset($pathinfo['extension']) ? $pathinfo['extension'] : null;
 		return $path;
 	}
 
 	public function filename(){ return $this->filename; }
 	public function controller(){ return $this->controller; }
 	public function args(){ return $this->args; }
+	public function format(){ return $this->format; }
+	public function force_format($format){ $this->format = $format; }
 }
