af02d7a2c549e15cb868fd61f0fb5ea38ac7e9c8
David Sveningsson <ext@sidvind.com>
2015-10-10 16:47:38 +0200

    cosmetic

--- view/admin/index.php
+++ view/admin/index.php
@@ -1,21 +1,26 @@
 <h2> Administration - Var försiktig! </h2>
-<p> Välkommen till administrationssidan, du kan välja vad du vill göra nedan: </p>
-<div class="list-group">
-        <?php if(has_right('Sido-moderator')) { ?>
-            <a class="list-group-item" href="<?=$root?>/admin/edit"> Redigera sidor </a>
-        <?php } ?>
 
-        <?php if(has_right('Bild-moderator')) { ?>
-            <a class="list-group-item" href="<?=$root?>/admin/images"> Hantera bilder </a>
-        <?php } ?>
+<div class="row">
+	<div class="col-sm-6">
+		<p> Välkommen till administrationssidan, du kan välja vad du vill göra nedan: </p>
+		<div class="list-group">
+			<?php if(has_right('Sido-moderator')): ?>
+				<a class="list-group-item" href="<?=$root?>/admin/edit"> Redigera sidor </a>
+			<?php endif; ?>
 
-        <?php if(has_right('Nyhets-moderator')) { ?>
-            <a class="list-group-item" href="<?=$root?>/admin/news"> Hantera nyheter </a>
-        <?php } ?>
+			<?php if(has_right('Bild-moderator')): ?>
+				<a class="list-group-item" href="<?=$root?>/admin/images"> Hantera bilder </a>
+			<?php endif; ?>
 
-        <?php if(has_right('Schema-moderator')) { ?>
-            <a class="list-group-item" href="<?=$root?>/admin/timetable"> Hantera schema </a>
-        <?php } ?>
+			<?php if(has_right('Nyhets-moderator')): ?>
+				<a class="list-group-item" href="<?=$root?>/admin/news"> Hantera nyheter </a>
+			<?php endif; ?>
+
+			<?php if(has_right('Schema-moderator')): ?>
+				<a class="list-group-item" href="<?=$root?>/admin/timetable"> Hantera schema </a>
+			<?php endif; ?>
+		</div>
+	</div>
 </div>
 
 <h3> Förklaringar: </h3>
