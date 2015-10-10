a3e37012bfeef80e8d863dd5b99bdeab6c728b2c
David Sveningsson <ext@sidvind.com>
2015-10-10 14:04:56 +0000

    mobile initial scale

--- index.php
+++ index.php
@@ -56,7 +56,9 @@
 <html lang="sv">
 	<head>
 		<title> NitroXy <?=$event?> - Info </title>
-		<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<script>
 			var root = '<?=$root?>';
 		</script>
