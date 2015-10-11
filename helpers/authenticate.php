35667d4dda8376f08783af38759d648f0481e9de
David Sveningsson <ext@sidvind.com>
2015-10-11 04:28:40 +0200

    cosmetic

--- helpers/authenticate.php
+++ helpers/authenticate.php
@@ -1,7 +1,7 @@
 <?php
 
 function is_loggedin() {
-    return NXAuth::is_authenticated();
+	return NXAuth::is_authenticated();
 }
 
 function is_admin() {
@@ -29,64 +29,64 @@ function is_admin() {
 }
 
 function ensure_right($right) {
-    if(!has_right($right)) {
-        throw new HTTPError403();
-    }
+	if(!has_right($right)) {
+		throw new HTTPError403();
+	}
 }
 
 function has_right($right) {
-    if(is_admin()) {
-        return true;
-    }
+	if(is_admin()) {
+		return true;
+	}
 
-    $rs = get_rights();
-    if(!isset($rs)) {
-        return false;
-    }
+	$rs = get_rights();
+	if(!isset($rs)) {
+		return false;
+	}
 
-    foreach($rs as $r) {
-        if($right == "Administratör") {
-            return true; //Has all rights .. uglyhack :D
-        }
-        if($right == $r) {
-            return true;
-        }
-    }
-    return false;
+	foreach($rs as $r) {
+		if($right == "Administratör") {
+			return true; //Has all rights .. uglyhack :D
+		}
+		if($right == $r) {
+			return true;
+		}
+	}
+	return false;
 }
 
 function get_rights() {
-    $u = NXAuth::user();
+	$u = NXAuth::user();
 
-    $rights = Right::from_username($u->username);
-    if(!isset($rights)) {
-        return null;
-    }
+	$rights = Right::from_username($u->username);
+	if(!isset($rights)) {
+		return null;
+	}
 
-    $ret = array();
-    foreach($_rights as $right => $bit) {
-        if($rights->permissions & (1 << $bit)) {
-            $ret[] = $right;
-        }
-    }
-    return $ret;
+	$ret = array();
+	foreach($_rights as $right => $bit) {
+		if($rights->permissions & (1 << $bit)) {
+			$ret[] = $right;
+		}
+	}
+	return $ret;
 }
 
 /* no idea why I put it here ... */
 function postdata($s) {
-    return isset($_POST[$s]) ? $_POST[$s] : null;
+	return isset($_POST[$s]) ? $_POST[$s] : null;
 }
 
 function getdata($s) {
-    return isset($_GET[$s]) ? $_GET[$s] : null;
+	return isset($_GET[$s]) ? $_GET[$s] : null;
 }
 
 function is_post() {
-    return (strcasecmp($_SERVER['REQUEST_METHOD'], 'POST') == 0);
+	return (strcasecmp($_SERVER['REQUEST_METHOD'], 'POST') == 0);
 }
 
 function ensure_post() {
-    if(!is_post()) {
-        throw new HTTPError403();
-    }
+	if(!is_post()) {
+		throw new HTTPError403();
+	}
 }
