8b6821734ae5c980f5b01b662344775177a859ff
David Sveningsson <ext@sidvind.com>
2015-10-11 23:38:29 +0200

    page: fix delete

--- view/admin/page/delete.php
+++ view/admin/page/delete.php
@@ -1,6 +1,6 @@
 <h2> Ta bort sida </h2>
 <p> Vill du verkligen ta bort sidan "<?=$s->display_name?>"? </p>
-<form method="post" action="/admin/delete/<?=$s->id?>">
+<form method="post" action="<?=url('/admin/delete/:id', $s->id)?>">
 	<input class="btn btn-danger" type="submit" class="button" value="Ja"/>
-        <a class="btn btn-success" href="/admin/edit/<?=$s->id?>">Nej</a>
+	<a class="btn btn-success" href="<?=url('/admin/edit/:id', $s->id)?>">Nej</a>
 </form>
